ee4e91494c79c6a2ec81e21c69afc9bc5bb0478f
ardok-m <carlosgarcia@iff.csic.es>
2019-03-14 14:00:58 +0100

    compute workspaces

---
+++ run_flat_2b_sims.py
@@ -0,0 +1,217 @@
+from __future__ import print_function
+from optparse import OptionParser
+import pymaster as nmt
+import numpy as np
+import matplotlib.pyplot as plt
+import flatmaps as fm
+import templates as tp
+import os
+import sys
+
+def opt_callback(option, opt, value, parser):
+        setattr(parser.values, option.dest, value.split(','))
+parser = OptionParser()
+parser.add_option('--prefix-out',dest='prefix_out',default='run',type=str,
+                  help='Output prefix')
+parser.add_option('--isim-ini', dest='isim_ini', default=1, type=int,
+                  help='Index of first simulation')
+parser.add_option('--isim-end', dest='isim_end', default=100, type=int,
+                  help='Index of last simulation')
+parser.add_option('--nls-contaminants', dest='nls_cont', default=0, type=int,
+                  help='Number of Large Scales contaminants')
+parser.add_option('--nss-contaminants', dest='nss_cont', default=0, type=int,
+                  help='Number of Small Scales contaminants')
+parser.add_option('--wo-contaminants', dest='wo_cont', default=False, action='store_true',
+                  help='Set if you don\'t want to use contaminants (ignore for now)')
+parser.add_option('--plot', dest='plot_stuff', default=False, action='store_true',
+                  help='Set if you want to produce plots')
+parser.add_option('--no-deproject',dest='no_deproject',default=False,action='store_true',
+                  help='Set if you will include contaminants but won\'t clean them (ignore for now)')
+parser.add_option('--no-debias',dest='no_debias',default=False,action='store_true',
+                  help='Set if you will include contaminants, clean them but won\'t correct for the bias (ignore for now)')
+parser.add_option('--low-noise-ee-bb',dest='low_noise_ee_bb',default=False,action='store_true',
+                  help='Set if you want the noise for ee and bb modes be multiplied by 1e-2')
+(o, args) = parser.parse_args()
+
+nsims=o.isim_end-o.isim_ini+1
+
+#Read input power spectra
+f = np.load("data/cls_lss_2bins.npz")
+l = f['ls']
+cls_ar = f['cls'][np.triu_indices(f['cls'].shape[0])]
+nls_ar = f['nls'][np.triu_indices(f['cls'].shape[0])]
+
+if o.low_noise_ee_bb:
+    nlee *= 1e-2
+    nlbb *= 1e-2
+
+if o.plot_stuff :
+    plt.figure()
+    plt.plot(l,cltt,'r-',label='TT')
+    plt.plot(l,clee,'b-',label='EE')
+    plt.plot(l,clbb,'g-',label='BB')
+    plt.plot(l,clte,'y-',label='TE')
+    plt.loglog()
+    plt.legend()
+
+#Read mask
+fmi,mask_hsc=fm.read_flat_map("data/mask_lss_flat.fits")
+if o.plot_stuff :
+    fmi.view_map(mask_hsc)
+
+#Set up binning scheme
+ell_min=max(2*np.pi/fmi.lx_rad,2*np.pi/fmi.ly_rad)
+ell_max=min(fmi.nx*np.pi/fmi.lx_rad,fmi.ny*np.pi/fmi.ly_rad)
+d_ell=2*ell_min
+n_ell=int((ell_max-ell_min)/d_ell)-1
+l_bpw=np.zeros([2,n_ell])
+l_bpw[0,:]=ell_min+np.arange(n_ell)*d_ell
+l_bpw[1,:]=l_bpw[0,:]+d_ell
+b=nmt.NmtBinFlat(l_bpw[0,:],l_bpw[1,:])
+
+#Generate an initial simulation
+def get_fields(fsk,mask,w_cont=False) :
+    """
+    Generate a simulated field.
+    It returns two NmtField objects for a spin-0 and a spin-2 field.
+
+    :param fsk: a fm.FlatMapInfo object.
+    :param mask: a sky mask.
+    :param w_cont: deproject any contaminants? (not implemented yet)
+    """
+    nbins = 2
+    spins = [0,2] * nbins
+    # maps == [st1, sq1, su1, st2, sq2, su2, ...] (oredered as in spins)
+    maps = nmt.synfast_flat(int(fsk.nx),int(fsk.ny),fsk.lx_rad,fsk.ly_rad,
+                              cls_ar + nls_ar, spins)
+    st1, sq1, su1, st2, sq2, su2 = maps
+
+    if w_cont :
+        raise ValueError('Contaminants not implemented yet')
+        # if np.any(templates_all):
+        #     tst, tsq, tsu = templates_all.sum(axis=0)
+        #     st+=tst; sq+=tsq; su+=tsu;
+        # if o.no_deproject :
+        #     ff0=nmt.NmtFieldFlat(fsk.lx_rad,fsk.ly_rad,mask.reshape([fsk.ny,fsk.nx]),
+        #                          [st])
+        #     ff2=nmt.NmtFieldFlat(fsk.lx_rad,fsk.ly_rad,mask.reshape([fsk.ny,fsk.nx]),
+        #                          [sq, su])
+        # else :
+        #     ff0=nmt.NmtFieldFlat(fsk.lx_rad,fsk.ly_rad,mask.reshape([fsk.ny,fsk.nx]),
+        #                          [st],
+        #                          templates=templates_all[:,0,None,:])
+        #     ff2=nmt.NmtFieldFlat(fsk.lx_rad,fsk.ly_rad,mask.reshape([fsk.ny,fsk.nx]),
+        #                          [sq,su],
+        #                          templates=templates_all[:,1:, :])
+    else :
+        ff0_1=nmt.NmtFieldFlat(fsk.lx_rad, fsk.ly_rad, mask.reshape([fsk.ny, fsk.nx]),
+                             [st1])
+        ff0_2=nmt.NmtFieldFlat(fsk.lx_rad, fsk.ly_rad, mask.reshape([fsk.ny, fsk.nx]),
+                             [st2])
+        ff2_1=nmt.NmtFieldFlat(fsk.lx_rad, fsk.ly_rad, mask.reshape([fsk.ny, fsk.nx]),
+                             [sq1, su1])
+        ff2_2=nmt.NmtFieldFlat(fsk.lx_rad, fsk.ly_rad, mask.reshape([fsk.ny, fsk.nx]),
+                             [sq2, su2])
+    return (ff0_1,ff2_1), (ff0_2, ff2_2)
+
+def get_coupling_matrix(w, f0, f1, suffix):
+    """
+    :param w:  NmtWorkspaceFlat initialized
+    :param f0: field 0 to correlate with f1
+    :param f1: field 1 to correlate with f0
+    :param suffix: field suffix where coupling matrix is stored.
+    """
+    if not os.path.isfile(o.prefix_out + suffix):
+        w.compute_coupling_matrix(f0, f1, b)
+        w.write_to(o.prefix_out + suffix);
+    else:
+        w.read_from(o.prefix_out + suffix)
+
+def get_workspaces(fields):
+    """
+    :param fields: tuple of tuple of fields to compute the mode-coupling matrix for. Shape is nbis x (f0, f2)
+    """
+    spins = [0, 2] * len(fields)
+
+    ws = []
+
+    fields = sum(fields, ())  # Flatten the tuple of tuples
+
+    zbin1 = 1
+    c1 = 0
+    for f1, s1 in zip(fields, spins):
+        c2 = c1
+        zbin2 = int(c2 / 2) + 1
+
+        for f2, s2 in zip(fields[c1:], spins[c1:]):
+            print("Computing {}{}_{}{}".format(s1, s2, zbin1, zbin2))
+            suffix = "_w{}{}_{}{}.dat".format(s1, s2, zbin1, zbin2)
+            w = nmt.NmtWorkspaceFlat()
+            get_coupling_matrix(w, f1, f2, suffix)
+            ws.append(w)
+            if not ((c2 + 1) % 2):
+                zbin2 += 1
+            c2 += 1
+
+        if not ((c1 + 1) % 2):
+            zbin1 += 1
+
+        c1 +=1
+
+    return ws
+
+############## Generate fields #####################
+np.random.seed(1000)
+fields = get_fields(fmi, mask_hsc) #, o.nss_cont or o.nls_cont)
+fbin1,  fbin2 = fields
+workspaces = get_workspaces(fields)
+
+import sys
+sys.exit()
+
+##Generate theory prediction
+#if not os.path.isfile(o.prefix_out+'_cl_th.txt') :
+#    print("Computing theory prediction")
+#    cl00_th=w00.decouple_cell(w00.couple_cell(l,np.array([cltt])))
+#    cl02_th=w02.decouple_cell(w02.couple_cell(l,np.array([clte,0*clte])))
+#    cl22_th=w22.decouple_cell(w22.couple_cell(l,np.array([clee,0*clee,0*clbb,clbb])))
+#    np.savetxt(o.prefix_out+"_cl_th.txt",
+#               np.transpose([b.get_effective_ells(),cl00_th[0],cl02_th[0],cl02_th[1],
+#                             cl22_th[0],cl22_th[1],cl22_th[2],cl22_th[3]]))
+#else :
+#    cl00_th=np.zeros([1,b.get_n_bands()])
+#    cl02_th=np.zeros([2,b.get_n_bands()])
+#    cl22_th=np.zeros([4,b.get_n_bands()])
+#    dum,cl00_th[0],cl02_th[0],cl02_th[1],cl22_th[0],cl22_th[1],cl22_th[2],cl22_th[3]=np.loadtxt(o.prefix_out+"_cl_th.txt",unpack=True)
+
+
+#Compute mean and variance over nsims simulations
+cl00_all=[]
+cl02_all=[]
+cl22_all=[]
+for i in np.arange(nsims) :
+    #if i%100==0 :
+    print("%d-th sim"%(i+o.isim_ini))
+
+    if not os.path.isfile(o.prefix_out+"_cl_%04d.npz"%(o.isim_ini+i)) :
+        f0,f2=get_fields(fmi,mask_hsc)
+        cl00=w00.decouple_cell(nmt.compute_coupled_cell_flat(f0,f0,b))#,cl_bias=clb00)
+        cl02=w02.decouple_cell(nmt.compute_coupled_cell_flat(f0,f2,b))#,cl_bias=clb02)
+        cl22=w22.decouple_cell(nmt.compute_coupled_cell_flat(f2,f2,b))#,cl_bias=clb22)
+        np.savez(o.prefix_out+"_cl_%04d"%(o.isim_ini+i),
+                 l=b.get_effective_ells(),cltt=cl00[0],clte=cl02[0],cltb=cl02[1],
+                 clee=cl22[0],cleb=cl22[1],clbe=cl22[2],clbb=cl22[3])
+    cld=np.load(o.prefix_out+"_cl_%04d.npz"%(o.isim_ini+i))
+    cl00_all.append([cld['cltt']])
+    cl02_all.append([cld['clte'],cld['cltb']])
+    cl22_all.append([cld['clee'],cld['cleb'],cld['clbe'],cld['clbb']])
+cl00_all=np.array(cl00_all)
+cl02_all=np.array(cl02_all)
+cl22_all=np.array(cl22_all)
+
+#Save output
+np.savez(o.prefix_out+'_clsims_%04d-%04d'%(o.isim_ini,o.isim_end),
+         l=b.get_effective_ells(),cl00=cl00_all,cl02=cl02_all,cl22=cl22_all)
+
+if o.plot_stuff :
+    plt.show()
