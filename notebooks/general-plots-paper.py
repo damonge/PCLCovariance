0fc76c77153ae770bbd076338b90e1285f81eae7
ardok-m <carlosgarcia@iff.csic.es>
2019-05-23 12:14:16 +0100

    commented-out lines for comparison 1bin th cls + change name of diff corr matrix

--- notebooks/general-plots-paper.py
+++ notebooks/general-plots-paper.py
@@ -103,50 +103,50 @@ plt.close()
 ########################## Compare 1bin sph Cls ##############################
 ##############################################################################
 
-fname = os.path.join(outdir, 'compare-sph-cls-used-1bin.pdf')
-
-clTh = np.loadtxt('./data/cls_lss.txt', unpack=True)
-lTh = clTh[0][:3*512]
-clTh_TT = (clTh[1] + clTh[5])[:3*512]
-clTh_EE = (clTh[2] + clTh[6])[:3*512]
-clTh_TE = (clTh[4] + clTh[-1])[:3*512]
-
-f, axs = plt.subplots(1, 3, figsize=(8, 3), gridspec_kw={'hspace': 0, 'wspace': 0}, sharex=True, sharey='row')
-
-cl2bins_1 = (cl2bin + nls2bin)[:3, :3]
-cl2bins_2 = (cl2bin + nls2bin)[3:, 3:]
-i, j = np.triu_indices(3)
-
-#### TT #####
-axs[0].loglog(lTh, clTh_TT, label='1-bin Fid.')
-axs[0].loglog(ell, cl2bins_1[0, 0], label='1st 2-bin Fid.', ls='--')
-axs[0].loglog(ell, cl2bins_2[0, 0], label='2nd 2-bin Fid.', ls='--')
-axs[0].set_ylabel(r'$C_l$')
-
-axs[0].text(0.5, 0.9, r'$C_l^{gg}$', transform=axs[0].transAxes)
-
-#### TE #####
-axs[1].loglog(lTh, clTh_TE, label='1-bin Fid.')
-axs[1].loglog(ell, cl2bins_1[0, 1], label='1st 2-bin Fid.', ls='--')
-axs[1].loglog(ell, cl2bins_2[0, 1], label='2nd 2-bin Fid.', ls='--')
-axs[1].text(0.5, 0.9, r'$C_l^{g\gamma}$', transform=axs[1].transAxes)
-#axs[1].set_ylabel(r'$C_l^{g\gamma}$')
-
-#### EE #####
-axs[2].loglog(lTh, clTh_EE, label='1-bin Fid.')
-axs[2].loglog(ell, cl2bins_1[1, 1], label='1st 2-bin Fid.', ls='--')
-axs[2].loglog(ell, cl2bins_2[1, 1], label='2nd 2-bin Fid.', ls='--')
-axs[2].text(0.5, 0.9, r'$C_l^{\gamma\gamma}$', transform=axs[2].transAxes)
-#axs[2].set_ylabel(r'$C_l^{\gamma\gamma}$')
-
-axs[0].legend(loc=0)
-
-for i in range(axs.shape[0]):
-    axs[i].set_xlabel('l')
-
-plt.tight_layout()
-plt.savefig(fname, dpi=DPI)
-plt.close()
+# fname = os.path.join(outdir, 'compare-sph-cls-used-1bin.pdf')
+#
+# clTh = np.loadtxt('./data/cls_lss.txt', unpack=True)
+# lTh = clTh[0][:3*512]
+# clTh_TT = (clTh[1] + clTh[5])[:3*512]
+# clTh_EE = (clTh[2] + clTh[6])[:3*512]
+# clTh_TE = (clTh[4] + clTh[-1])[:3*512]
+#
+# f, axs = plt.subplots(1, 3, figsize=(8, 3), gridspec_kw={'hspace': 0, 'wspace': 0}, sharex=True, sharey='row')
+#
+# cl2bins_1 = (cl2bin + nls2bin)[:3, :3]
+# cl2bins_2 = (cl2bin + nls2bin)[3:, 3:]
+# i, j = np.triu_indices(3)
+#
+# #### TT #####
+# axs[0].loglog(lTh, clTh_TT, label='1-bin Fid.')
+# axs[0].loglog(ell, cl2bins_1[0, 0], label='1st 2-bin Fid.', ls='--')
+# axs[0].loglog(ell, cl2bins_2[0, 0], label='2nd 2-bin Fid.', ls='--')
+# axs[0].set_ylabel(r'$C_l$')
+#
+# axs[0].text(0.5, 0.9, r'$C_l^{gg}$', transform=axs[0].transAxes)
+#
+# #### TE #####
+# axs[1].loglog(lTh, clTh_TE, label='1-bin Fid.')
+# axs[1].loglog(ell, cl2bins_1[0, 1], label='1st 2-bin Fid.', ls='--')
+# axs[1].loglog(ell, cl2bins_2[0, 1], label='2nd 2-bin Fid.', ls='--')
+# axs[1].text(0.5, 0.9, r'$C_l^{g\gamma}$', transform=axs[1].transAxes)
+# #axs[1].set_ylabel(r'$C_l^{g\gamma}$')
+#
+# #### EE #####
+# axs[2].loglog(lTh, clTh_EE, label='1-bin Fid.')
+# axs[2].loglog(ell, cl2bins_1[1, 1], label='1st 2-bin Fid.', ls='--')
+# axs[2].loglog(ell, cl2bins_2[1, 1], label='2nd 2-bin Fid.', ls='--')
+# axs[2].text(0.5, 0.9, r'$C_l^{\gamma\gamma}$', transform=axs[2].transAxes)
+# #axs[2].set_ylabel(r'$C_l^{\gamma\gamma}$')
+#
+# axs[0].legend(loc=0)
+#
+# for i in range(axs.shape[0]):
+#     axs[i].set_xlabel('l')
+#
+# plt.tight_layout()
+# plt.savefig(fname, dpi=DPI)
+# plt.close()
 
 ##############################################################################
 ############################# Mask sph1 ######################################
@@ -210,10 +210,6 @@ plt.close()
 cl_ss = tp.create_cl_templates(ell, cl2bin[0, 0] + nls2bin[0, 0], exp_range=(0, 0), N=1)
 cl_ls1 = tp.create_cl_templates(ell, cl2bin[0, 0] + nls2bin[0, 0], exp_range=(-3, -3), N=1)[0]
 cl_ls2 = tp.create_cl_templates(ell, cl2bin[0, 0] + nls2bin[0, 0], exp_range=(-1, -1), N=1)[0]
-
-# cl_ss = tp.create_cl_templates(lTh, clTh_TT,  exp_range=(0, 0), N=1)
-# cl_ls1 = tp.create_cl_templates(lTh, clTh_TT, exp_range=(-3, -3), N=1)[0]
-# cl_ls2 = tp.create_cl_templates(lTh, clTh_TT, exp_range=(-1, -1), N=1)[0]
 
 fname = os.path.join(outdir, 'foreground.pdf')
 
@@ -309,7 +305,7 @@ ax.set_xticklabels(labels_cov)
 ax.set_yticks(ticks)
 ax.set_yticklabels(labels_cov)
 
-fname = os.path.join(outdir, 'sph_2bin_diff_corr.pdf')
+fname = os.path.join(outdir, 'run_sph_2b_same_mask_NKA_diff_corr.pdf')
 plt.savefig(fname, dpi=DPI)
 # plt.show()
 plt.close()
